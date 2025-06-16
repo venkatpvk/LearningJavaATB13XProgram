67ef375280a21396691b55afd47eab79e9d8db9d
Pothineni <venkatkrishna.pothineni@gmail.com>
2025-06-16 19:30:07 +0530

    June 16th Commit

---
+++ src/TriangleClasification.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TriangleClasification {
+    public static void main(String[] args) {
+        System.out.println("Please give 3 inputs");
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+
+        if (num1 == num2 && num2 == num3) {
+            System.out.println("All the 3 inputs are equal so it is a Equilateral Triangle ");
+        } else if (num1 == num2 || num1 == num3 || num2 == num3) {
+            System.out.println("Two sides are equal so it is Isosceles triangle");
+        } else {
+            System.out.println("No sides are equal so it is a scalene triangle");
+        }
+    }
+}
